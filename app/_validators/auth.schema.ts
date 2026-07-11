d564ecc395a7ff21e288ba09f3322e0afdddb6b6
Hp <rs739025@gmail.com>
2026-07-11 18:52:00 +0530

    signup form done

--- app/_validators/auth.schema.ts
+++ app/_validators/auth.schema.ts
@@ -2,11 +2,11 @@ import {z} from "zod";
 
 
 export const SignupSchema = z.object({
-    name: z.string().min(2, "Must at be 2 characters"),
+    name: z.string().trim().min(2, "Must at be 2 characters").max(25,"Name is too long..."),
     email: z.email("Please enter a valid email address."),
-    password: z.string().min(8, "Password must be at least 8 characters."),
-    confirmpassword: z.string(),
-}).refine((data) => data.password === data.confirmpassword , {
+    password: z.string().min(8, "Password must be at least 8 characters.").max(50,"Password is too long..."),
+    confirmPassword: z.string(),
+}).refine((data) => data.password === data.confirmPassword , {
     message: "Password Dosen't match",
     path: ["confirmpassword"]
 })
---
+++ components/forms/sign-up-form.tsx
@@ -0,0 +1,115 @@
+"use client"
+import { useState } from "react";
+import {authClient} from "@/app/lib/auth-client";
+import { SignupSchema } from "@/app/_validators/auth.schema";
+import { useRouter } from "next/navigation";
+import { Alert, AlertDescription } from "../ui/alert";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+
+export default function SignUpform() {
+    const router = useRouter()
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        root: ""
+    })      
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        setErrors({ name: "", email: "", password: "", confirmPassword: "", root: "" });
+        const validatedata = SignupSchema.safeParse(formData)
+        if(!validatedata.success){
+            const fieldErrors = validatedata.error.flatten().fieldErrors;
+
+            setErrors((prev) => ({
+                ...prev,
+                name: fieldErrors.name?.[0] || "",
+                email: fieldErrors.email?.[0] || "",
+                password: fieldErrors.password?.[0] || "",
+                confirmPassword: fieldErrors.confirmPassword?.[0] || "",
+            }))
+            return;
+        }
+        setLoading(true);
+
+        const {data , error} = await authClient.signUp.email({
+            name: validatedata.data.name,
+            email: validatedata.data.email,
+            password: validatedata.data.password,
+        })
+        if(error){
+            setErrors((prev) => ({...prev, root: error.message as string}));
+            setLoading(false);
+            return;
+        }
+        setLoading(false);
+        router.push("")
+    }
+    return(
+        <form 
+            action="handleSubmit"
+            className="flex flex-col space-y-5"
+            >
+            {errors.root && (
+                <Alert variant={"destructive"}>
+                    <AlertDescription>{errors.root}</AlertDescription>
+                </Alert>
+            )}
+            <div>
+                <Label htmlFor="name" className="text-left">Name</Label>
+                <Input 
+                    id="name"
+                    type="text"
+                    value={formData.name}
+                    onChange={(e) => setFormData((prev) => ({...prev, name : e.target.value}))}
+                    placeholder="Rahul"
+                />
+                {errors.name && <span className="text-sm font-medium text-destructive text-left">{errors.name}</span>}
+            </div>
+            <div>
+                <Label htmlFor="email" className="text-left">Email</Label>
+                <Input 
+                    id="email"
+                    type="email"
+                    value={formData.email}
+                    onChange={(e) => setFormData((prev) => ({...prev, email : e.target.value}))}
+                    placeholder="Rahul@gmail.com"
+                />
+                {errors.email && <span className="text-sm font-medium text-destructive text-left">{errors.email}</span>}
+            </div>
+            <div>
+                <Label htmlFor="password" className="text-left">Password</Label>
+                <Input 
+                    id="password"
+                    type="password"
+                    value={formData.password}
+                    onChange={(e) => setFormData((prev) => ({...prev, password : e.target.value}))}
+                    placeholder="******"
+                />
+                {errors.password && <span className="text-sm font-medium text-destructive text-left">{errors.password}</span>}
+            </div>
+            <div>
+                <Label htmlFor="confirmpassword" className="text-left">Confirm Password</Label>
+                <Input 
+                    id="confirm Password"
+                    type="password"
+                    value={formData.confirmPassword}
+                    onChange={(e) => setFormData((prev) => ({...prev, confirmPassword : e.target.value}))}
+                    placeholder=""
+                />
+                {errors.confirmPassword && <span className="text-sm font-medium text-destructive text-left">{errors.confirmPassword}</span>}
+            </div>
+        </form>
+    )
+}
